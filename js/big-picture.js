639bda745845549c87f3a95b60b31384afb868ea
Ramil Zaripov <zarram89@gmail.com>
2024-10-30 08:53:57 +0500

    feature/добавляет комментарии

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,8 +1,8 @@
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
 const commentCount = document.querySelector('.social__comment-count');
-const commentList = document.querySelector('.social__comments');
 const commentsLoader = document.querySelector('.comments-loader');
+const commentList = document.querySelector('.social__comments');
 const cancelButton = document.querySelector('.big-picture__cancel');
 
 const hideBigPicture = () => {
@@ -20,6 +20,30 @@ function onEscKeyDown(evt) {
 
 const onCancelButtonClick = () => hideBigPicture();
 
+const createComment = ({avatar, name, message}) => {
+  const comment = document.createElement('li');
+  comment.innerHTML = '<img class="social__picture" src="" alt="" width="35" height="35"><p class="social__text"></p>';
+  comment.classList.add('social__comment');
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+const renderComments = (comments) => {
+  commentList.innerHTML = '';
+
+  const fragment = document.createDocumentFragment();
+  comments.forEach((comment) => {
+    const commentElement = createComment(comment);
+    fragment.append(commentElement);
+  });
+
+  commentList.append(fragment);
+};
+
 const renderPictureDetails = ({url, description, likes}) => {
   bigPicture.querySelector('.big-picture__img img').src = url;
   bigPicture.querySelector('.big-picture__img img').alt = description;
@@ -35,6 +59,7 @@ const showBigPicture = (data) => {
   document.addEventListener('keydown', onEscKeyDown);
 
   renderPictureDetails(data);
+  renderComments(data.comments);
 };
 
 cancelButton.addEventListener('click', onCancelButtonClick);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { getPictures } from './data';
 import { renderPictures } from './picture';
 
+
 renderPictures(getPictures());
